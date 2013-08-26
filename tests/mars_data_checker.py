1bd35fd6bf3ac3c37cbdbef31c1e2edbad145f80
Conor Forde <sprkwear@gmail.com>
2013-08-25 19:14:36 -0700

    Update mars_data_checker.py
    encapsulated into main function and folder traversing fixed

--- tests/mars_data_checker.py
+++ tests/mars_data_checker.py
@@ -1,18 +1,27 @@
 """Runs pylint on mars_data.py with desired attributes"""
 import subprocess, os
 
-tested_file = "mars_data"
-tested_file_python = "../"+tested_file+".py"
-subprocess.call("pyreverse --project="+tested_file+"UML --filter-mode=ALL "+tested_file_python, 
-    shell=True)
+def main():
+    tested_file = "mars_data"
+    file_path = "../bin/"
+    uml_path = "../docs/uml/"
+    tested_file_python = file_path+tested_file+".py"
 
-subprocess.call("dot -Tpng classes_"+tested_file+"UML.dot -o "+tested_file+"UML.png", shell=True)
-os.remove("classes_"+tested_file+"UML.dot")
-os.rename(tested_file+"UML.png", "../uml/"+tested_file+"_uml.png")
+    subprocess.call("pyreverse --project="+tested_file+"UML --filter-mode=ALL "+tested_file_python,
+        shell=True)
 
-subprocess.call("pylint --const-rgx='[a-z_][a-z0-9_]{2,30}$'" \
-    " --disable=RP0401 --disable=RP0001 --disable=RP0002 " \
-    "--disable=RP0101 --disable=RP0701 --disable=RP0801 "+tested_file_python, shell=True)
+    subprocess.call("dot -Tpng classes_"+tested_file+"UML.dot -o "+tested_file+"UML.png", shell=True)
+    os.remove("classes_"+tested_file+"UML.dot")
+    os.rename(tested_file+"UML.png", uml_path+tested_file+"_uml.png")
 
-subprocess.call("coverage run "+tested_file_python, shell=True)
-subprocess.call("coverage report", shell=True)
+    subprocess.call("pylint --const-rgx='[a-z_][a-z0-9_]{2,30}$'" \
+        " --disable=RP0401 --disable=RP0001 --disable=RP0002 " \
+        "--disable=RP0101 --disable=RP0701 --disable=RP0801 "+tested_file_python, shell=True)
+
+    subprocess.call("coverage run "+tested_file_python, shell=True)
+    subprocess.call("coverage report", shell=True)
+
+    #subprocess.call("python -m unittest test_"+tested_file+".py")
+
+if __name__ == '__main__':
+    main()
