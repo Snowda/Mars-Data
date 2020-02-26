624bc65eee86b80eef5261337043a192a8ba2131
Snowda <conor@dusklab.com>
2020-02-26 00:31:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,26 +3,23 @@
 import os
 from setuptools import setup, find_packages
 
-# Utility function to read the README file.
-# Used for the long_description. It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
+# Utility function to read README file to create the long_description.
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "'''+self.project_path+'''",
-    version = "0.0.1",
-    author = "Conor Forde",
-    author_email = "me@conorforde",
-    description = (""),
-    license = "LGPL3",
-    keywords = "martian data accumulator",
-    url = "",
+    name="'''+self.project_path+'''",
+    version="0.1.0",
+    author="Conor Forde",
+    author_email="me@conorforde",
+    description=(""),
+    license="LGPL3",
+    keywords="martian data accumulator",
+    url="",
     packages=find_packages(),
     long_description=read('README'),
     classifiers=[
-        "Development Status :: 1 - Alpha",
+        "Development Status :: 0.1.0 - Alpha",
         "Topic :: Utilities",
         "License :: LGPL3 License",
     ],
