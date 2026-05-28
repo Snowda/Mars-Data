599caa2a1f1706921021adabf4769c97bb370828
Snowda <conor@dusklab.com>
2026-05-28 18:28:41 -0400

    Removing need for Config

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,22 @@
 mod mars;
 
 use mars::calculations::{mars_comms_delay, mars_comms_return_delay};
-use mars::config::load_config;
 use mars::weather::load_weather_data;
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() {
-    println!("Mars communications delay: {} seconds", mars_comms_delay(None));
-    println!("Mars round-trip communications delay: {} seconds", mars_comms_return_delay(None));
-    
-    match load_config("config.json") {
-        Ok(api_key) => {
-            match load_weather_data(&api_key).await {
-                Ok(weather_data) => {
-                    let _ = weather_data.print_report();
-                },
-                Err(e) => {
-                    eprintln!("Error loading weather data: {}", e);
-                    return;
-                }
-            };
+    tracing_subscriber::fmt::init();
+
+    info!("Mars communications delay: {} seconds", mars_comms_delay(None));
+    info!("Mars round-trip communications delay: {} seconds", mars_comms_return_delay(None));
+
+    match load_weather_data().await {
+        Ok(weather_data) => {
+            let _ = weather_data.print_report();
         },
         Err(e) => {
-            eprintln!("Error loading config: {}", e);
+            error!("Error loading weather data: {}", e);
             return;
         }
     };
